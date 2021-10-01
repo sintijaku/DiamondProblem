8c3ef4435df37a12b9d132d74e556e19a9c743ba
Sintija Kurtisa <sintija.kurtisa@gmail.com>
2021-10-01 12:34:06 +0300

    Add project files.

---
+++ DiamondProblem/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DiamondProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {}
+    }
+
+    public class DiamondCreator
+    {
+        public List<string> CreateDiamond(string letter)
+        {
+            var alphabetDictionary = CreateDictionary();
+            var widestPoint = alphabetDictionary.Where(x => x.Value == letter.ToString()).Select(y => y.Key).FirstOrDefault();
+            int innerSpaces = 0;
+            int outerSpaces = widestPoint;
+            var diamondHalf = new List<string>();
+
+            if (widestPoint == 0)
+            {
+                return new List<string>() { "A" };
+            }
+            else
+            {
+                for (int i = 0; i <= widestPoint; i++)
+                {
+                    if (i == 0)
+                    {
+                        diamondHalf.Add($"{CreateSpaces(outerSpaces)}{alphabetDictionary[i]}{CreateSpaces(outerSpaces)}");
+                    }
+                    else
+                    {
+                        diamondHalf.Add($"{CreateSpaces(outerSpaces)}{alphabetDictionary[i]}{CreateSpaces(innerSpaces)}{alphabetDictionary[i]}{CreateSpaces(outerSpaces)}");
+                    }
+
+                    innerSpaces = innerSpaces >= 1 ? innerSpaces + 2 : innerSpaces + 1;
+                    outerSpaces -= 1;
+                }
+            }
+
+            return ComposeFullDiamond(diamondHalf);
+        }
+
+        private string CreateSpaces(int numberOfSpaces)
+        {
+            var spaces = string.Empty;
+            for (int i = 0; i < numberOfSpaces; i++)
+            {
+                spaces += " "; 
+            }
+            
+            return spaces;
+        }
+
+        private List<string> ComposeFullDiamond(List<string> diamondHalf)
+        {
+            var result = new List<string>();
+            result.AddRange(diamondHalf);
+            diamondHalf.Reverse();
+            diamondHalf.RemoveAt(0);
+            result.AddRange(diamondHalf);
+
+            return result;
+        }
+
+        private Dictionary<int, string> CreateDictionary()
+        {
+            var alphabetDictionary = new Dictionary<int, string>();
+
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                int key = i - 'A';
+                alphabetDictionary.Add(key, i.ToString());
+            }
+
+            return alphabetDictionary;
+        }
+    }
+}
---
+++ DiamondProblemTests/DiamondCreatorTests.cs
@@ -0,0 +1,167 @@
+using DiamondProblem;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiamondProblemTests
+{
+    public class Tests
+    {
+        private DiamondCreator _diamondCreator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _diamondCreator = new DiamondCreator();
+        }
+
+        [Test]
+        public void DiamondCreator_ProvidedLetterA_ReturnsA()
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond("A");
+
+            // Assert
+            Assert.AreEqual("A", result[0]);
+        }
+
+        [Test]
+        public void DiamondCreate_ProvidedLetterB_ReturnsLetterB_In_WidestPoint()
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond("B");
+
+            // Assert
+            Assert.That(result[1].Contains("B"));
+            Assert.That(result[1].Count(x => x == 'B') == 2);
+        }
+
+        [Test]
+        public void DiamondCreate_ProvidedLetterB_FirstAndLastLineContainLetterA()
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond("B");
+
+            // Assert
+            Assert.That(result[0].Contains("A"));
+            Assert.That(result[2].Contains("A"));
+        }
+
+        [Test]
+        public void DiamondCreate_ProvidedLetterB_PrintsCorrectDiamondShape()
+        {
+            // Arrange
+            var expectedResult = new List<string>()
+            {
+                " A ",
+                "B B",
+                " A "
+            };
+
+            // Act
+            var result = _diamondCreator.CreateDiamond("B");
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void DiamondCreate_ProvidedLetterC_FirstAndLastLineContainLetterA()
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond("C");
+
+            // Assert
+            result[0].Should().Contain("A");
+            result[4].Should().Contain("A");
+        }
+
+        [Test]
+        public void DiamondCreate_ProvidedLetterC_WidestPointContainsC()
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond("C");
+
+            // Assert
+            Assert.That(result[2].Contains("C"));
+            Assert.That(result[2].Count(x => x == 'C') == 2);
+        }
+
+        [Test]
+        public void DiamondCreate_ProvidedLetterD_PrintsDiamondShape()
+        {
+            // Arrange
+            var expectedResult = new List<string>()
+            {
+                "   A   ",
+                "  B B  ",
+                " C   C ",
+                "D     D",
+                " C   C ",
+                "  B B  ",
+                "   A   "
+            };
+
+            // Act
+            var result = _diamondCreator.CreateDiamond("D");
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("A", 0)]
+        [TestCase("B", 2)]
+        [TestCase("C", 4)]
+        [TestCase("H", 14)]
+        public void DiamondCreate_ProvidedDifferentLetters_PrintsCorrectCountOfSpacesForFirstLine(string letter, int expectedResult)
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond(letter);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result[0].Count(x => x == ' '));
+        }
+
+        [TestCase("A", 0)]
+        [TestCase("B", 1)]
+        [TestCase("C", 3)]
+        [TestCase("H", 13)]
+        public void DiamondCreate_ProvidedDifferentLetters_PrintsCorrectCountOfSpacesForMiddleLine(string letter, int expectedResult)
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond(letter);
+            var middleLineIndex = (result.Count - 1) / 2;
+
+            // Assert
+            Assert.AreEqual(expectedResult, result[middleLineIndex].Count(x => x == ' '));
+        }
+
+        [TestCase("A", 1)]
+        [TestCase("B", 3)]
+        [TestCase("C", 5)]
+        [TestCase("H", 15)]
+        public void DiamondCreate_ProvidedDifferentLetters_PrintsCorrectCountOfLines(string letter, int expectedResult)
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond(letter);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result.Count());
+        }
+
+        [TestCase("A", true)]
+        [TestCase("B", true)]
+        [TestCase("C", true)]
+        [TestCase("H", true)]
+        public void DiamondCreate_ProvidedDifferentLetters_PrintsCorrectLetterInWidestPoint(string letter, bool expectedResult)
+        {
+            // Act
+            var result = _diamondCreator.CreateDiamond(letter);
+            var middleLineIndex = (result.Count - 1) / 2;
+
+            // Assert
+            Assert.AreEqual(expectedResult, result[middleLineIndex].Contains(letter));
+        }
+    }
+}
